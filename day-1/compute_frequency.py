4a9be770e086259d643ada342412ec31c02d9afa
davidtranaeus <davidtra@kth.se>
2018-12-03 09:13:12 +0100

    Day 1 task 2

--- day-1/compute_frequency.py
+++ day-1/compute_frequency.py
@@ -1,6 +1,6 @@
 import sys
 
-def compute_result(file_name):
+def compute_sum(file_name):
 	with open(file_name, 'r') as f:
 		final = 0
 		lines = f.readlines()
@@ -9,5 +9,28 @@ def compute_result(file_name):
 			final += int(l[1:]) if l[0] == '+' else -int(l[1:])
 	return final
 
+def repeated_freq(file_name):
+	with open(file_name, 'r') as f:
+		vals = []
+		lines = f.readlines()
+
+		for l in lines:
+			sign = l[0]
+			vals.append(int(l[1:]) if l[0] == '+' else -int(l[1:]))
+		
+		total = 0
+		i = 0
+		taken = dict()
+		while True:
+			total += vals[i % len(vals)]
+			if total in taken:
+				return total
+			else:
+				taken[total] = True
+				i += 1
+
+	return total
+
 if __name__ == "__main__":
-	print(compute_result(sys.argv[1]))
+	print('input.txt')
+	print('input.txt')
